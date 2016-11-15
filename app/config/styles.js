2ec1cf84ea77e9e7f9c3249f98d6c81a9c19c9d6
fergmac <f.macconnell@gmail.com>
2016-11-15 13:09:07 -0800

    add Oticon icons to NavigationLayout TabItems

---
+++ app/config/styles.js
@@ -0,0 +1,16 @@
+export const colors = {
+  nearBlack: '#111111',
+  mediumGrey: '#999999',
+  lightGrey: '#dddddd', // mostly for borders
+  transluscentGrey: 'rgba(0,0,0,0.2)',
+  transluscentWhite: 'rgba(255,255,255,0.8)',
+  offWhite: '#f1f1f1',
+  lightYellow: '#fcf2bb',
+  mediumYellow: '#ffd505',
+};
+
+export const typography = {
+  baseSize: 16,
+  fontMain: 'Courier',
+  fontAlt: 'Helvetica',
+};
--- app/navigation/NavigationLayout.js
+++ app/navigation/NavigationLayout.js
@@ -7,9 +7,19 @@ import {
 } from '@exponent/ex-navigation';
 
 import { Router } from './routes';
+import Icon from 'react-native-vector-icons/Octicons';
+import { colors } from '../config/styles';
 
 // Treat the TabScreen route like any other route -- you may want to set
 // it as the intiial route for a top-level StackNavigation
+
+//this is to set nav bar to white
+const defaultRouteConfig = {
+    navigationBar: {
+        backgroundColor: 'white',
+    }
+}
+
 
 class NavigationLayout extends Component {
     static route = {
@@ -17,6 +27,14 @@ class NavigationLayout extends Component {
             visible: false,
         }
     }
+
+    iconRenderer(isSelected, iconName) {
+        const color = isSelected ? 'black' : colors.mediumGrey
+        return <Icon name={iconName} size={24} color={color} />
+    }
+
+    // renderTitleFunction here
+
 
     render() {
         return (
@@ -27,6 +45,7 @@ class NavigationLayout extends Component {
                 <TabItem
                     id="recent"
                     title="Recent"
+                    renderIcon={(isSelected) => this.iconRenderer(isSelected, 'watch')}
                     // selectedStyle={styles.selectedTab}
                     // renderIcon={(isSelected) => <Image source={require('../images/recent.png')} />} 
                     >
@@ -34,42 +53,49 @@ class NavigationLayout extends Component {
                         id="recent"
                         navigatorUID="recent"
                         initialRoute={Router.getRoute('recent')}
+                        defaultRouteConfig={defaultRouteConfig}
                         />
                 </TabItem>
 
                 <TabItem
                     id="random"
                     title="Random"
+                    renderIcon={(isSelected) => this.iconRenderer(isSelected, 'smiley')}
                     // selectedStyle={styles.selectedTab}
                     // renderIcon={(isSelected) => <Image source={require('../images/random.png')} />}
                     >
                     <StackNavigation
                         id="random"
                         initialRoute={Router.getRoute('random')}
+                        defaultRouteConfig={defaultRouteConfig}
                         />
                 </TabItem>
 
                 <TabItem
                     id="faves"
                     title="Faves"
+                    renderIcon={(isSelected) => this.iconRenderer(isSelected, 'heart')}
                     // selectedStyle={styles.selectedTab}
                     // renderIcon={(isSelected) => <Image source={require('../images/faves.png')} />}
                     >
                     <StackNavigation
                         id="faves"
                         initialRoute={Router.getRoute('faves')}
+                        defaultRouteConfig={defaultRouteConfig}
                         />
                 </TabItem>
 
                 <TabItem
                     id="about"
                     title="About"
+                    renderIcon={(isSelected) => this.iconRenderer(isSelected, 'info')}
                     // selectedStyle={styles.selectedTab}
                     // renderIcon={(isSelected) => <Image source={require('../images/about.png')} />}
                     >
                     <StackNavigation
                         id="about"
                         initialRoute={Router.getRoute('about')}
+                        defaultRouteConfig={defaultRouteConfig}
                         />
                 </TabItem>
             </TabNavigation>
