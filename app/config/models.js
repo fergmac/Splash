148693799742818d9c329891b169f0a1f606d227
fergmac <f.macconnell@gmail.com>
2016-11-16 11:00:55 -0800

    add Realm and create a Faves schema

---
+++ app/config/models.js
@@ -0,0 +1,12 @@
+import Realm from 'realm';
+
+const Fave = {
+  name: 'Fave',
+  primaryKey: 'id',
+  properties: {
+    id: 'string',
+    faved_on: 'date',
+  }
+};
+
+export default new Realm({schema: [Fave]});
