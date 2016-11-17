89cbb18562e38718d4190d4ffb12bda888634c44
fergmac <f.macconnell@gmail.com>
2016-11-17 15:53:37 -0800

    add saveToCameraRoll, saveFave and, goToRecent methods as props to PhotoBox scene and update info.plist

--- app/lib/databaseHelpers.js
+++ app/lib/databaseHelpers.js
@@ -1,20 +1,27 @@
-import Realm from '../config/models';
+import realm from '../config/models';
 
+// photoId: string
+// checks to see if photo has been faved
+// returns boolean
 export const faved = (photoId) => {
-    const isItChecked = realm.objects('Fave').filtered('id == $0',  photoId)
-    isItchecked[0] ? true : false
+    const isChecked = realm.objects('Fave').filtered('id == $0',  photoId)
+    return isChecked[0]
 }
 
+// faved: boolean
+// photoId: string
+// saves photo as a fave in the db
 export const saveFave = (faved, photoId) => {
+    console.log(photoId)
     realm.write(() => {
         // let isStared = faves.filtered('id == $0',  favId);
         if (faved) {
             // photo is already faved then delete 
             let photo = realm.objects('Fave').filtered('id == $0',  photoId)
-            realm.delete(faves); 
+            realm.delete(faved); 
         } else {
             //otherwise add faved photo to database
-            realm.create('Fave', {id: 1, faved_on: new Date() });
+            realm.create('Fave', {id: photoId, faved_on: new Date()});
         }
     })
 }
--- app/scenes/PhotoBox/PhotoBox.js
+++ app/scenes/PhotoBox/PhotoBox.js
@@ -5,23 +5,38 @@ import {
     Text,
     StyleSheet,
     TouchableHighlight,
+    CameraRoll,
+    saveToCameraRoll,
+    Platform,
 } from 'react-native';
 import { styles } from './styles';
 import Icon from 'react-native-vector-icons/Octicons';
+import { faved, saveFave } from '../../lib/databaseHelpers';
+
 
 const PhotoBox = (props) => {
     return (
         <View style={styles.container} >
-            <Icon name='x' size={24} color='yellow' />
-            <TouchableHighlight onPress={}>
-                <Icon name='cloud-download' size={24} color='yellow' />
-            </TouchableHighlight>
-            <TouchableHighlight onPress={}>
-                <Icon name='star' size={24} color='yellow' />
-            </TouchableHighlight>
+            <View style={styles.exitButton}>
+                <TouchableHighlight onPress={props.goToRecent}>
+                    <Icon name='x' size={24} color='white' />
+                </TouchableHighlight>
+            </View>
+            <View style={styles.buttons}>
+                {Platform.OS === 'ios' &&
+                    <TouchableHighlight onPress={() => CameraRoll.saveToCameraRoll(props.photo.urls.raw, "photo")}>
+                        <Icon name='cloud-download' size={24} color='white' />
+                    </TouchableHighlight>
+                }
+                <TouchableHighlight onPress={props.callSaveFave}>
+                    <Icon name='star' size={24} color='white' />
+                </TouchableHighlight>
+            </View>
             <Image style={styles.image} source={{ uri: props.photo.urls.raw }} />
-            <Text style={styles.text} >{props.photo.likes}</Text>
-            <Text style={styles.text} >{props.photo.downloads}</Text>
+            <View style={styles.details}>
+                <Text style={styles.text} >Likes: {props.photo.likes}</Text>
+                <Text style={styles.text} >Downloads: {props.photo.downloads}</Text>
+            </View>
         </View>
     )
 }
