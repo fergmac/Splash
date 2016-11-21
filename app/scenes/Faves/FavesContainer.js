874ed2ccf2bfed5964b07e0a9501573ad17c7086
fergmac <f.macconnell@gmail.com>
2016-11-20 16:22:58 -0800

    addListener to FavesContainer constructor to listen for Fave changes

--- app/scenes/Faves/FavesContainer.js
+++ app/scenes/Faves/FavesContainer.js
@@ -13,7 +13,7 @@ import Loader from '../../components/Loader';
 import { getFavedPhotos } from '../../lib/databaseHelpers';
 import { getFavedPhotoData } from '../../lib/splashHelpers';
 import { Router } from '../../navigation/routes';
-
+import realm from '../../config/models';
 
 class FavesContainer extends Component {
 
@@ -37,49 +37,68 @@ class FavesContainer extends Component {
         this._goToPhotoBox = this._goToPhotoBox.bind(this)
         this.ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
 
-        this.state = {
-            dataSource: this.ds,
-            isLoading: true,
-        }
+        realm.addListener('change', (changes) => {
+            this.getFavedPhotoDataToJson()
+        })
+
+//         realm.objects('Fave').addListener((fave, changes) => {
+//             changes.insertions.forEach((index) => {
+//                 this.setState({
+//                     dataSource: this.ds.cloneWithRows(results)
+//                 })
+//         });
+//             changes.deletions.forEach((index) => {
+//             this.setState({
+//                     dataSource: this.ds.cloneWithRows(results)
+//                 })
+//         });
+  
+  
+// });
+
+this.state = {
+    dataSource: this.ds,
+    isLoading: true,
+}
 
     }
 
-    componentDidMount() {
-        //instead call method 
-        console.log("componentDidMount")
-        this.getFavedPhotoDataToJson();
+componentDidMount() {
+    //instead call method 
+    console.log("componentDidMount")
+    this.getFavedPhotoDataToJson();
+}
+componentDidUpdate() {
+    console.log("comonentDidUpdate")
+    if (this.state.dataSource && this.state.isLoading) {
+        console.log("isloading: false")
+        this.setState({ isLoading: false });
     }
-    componentDidUpdate() {
-        console.log("comonentDidUpdate")
-        if (this.state.dataSource && this.state.isLoading) {
-            console.log("isloading: false")
-            this.setState({ isLoading: false });
-        }
-    }
+}
 
 
-    render() {
-        console.log("render faves container")
-        if (this.state.isLoading) {
-            return (
-                <Loader />
-            );
-        } else {
-            return (
-                <Faves favedPhotos={this.state.dataSource} goToPhotoBox={this._goToPhotoBox}/>
-            )
-        }
+render() {
+    console.log("render faves container")
+    if (this.state.isLoading) {
+        return (
+            <Loader />
+        );
+    } else {
+        return (
+            <Faves favedPhotos={this.state.dataSource} goToPhotoBox={this._goToPhotoBox} />
+        )
     }
-    getFavedPhotoDataToJson() {
-        getFavedPhotoData(getFavedPhotos())
-            .then(results => {
-                console.log(results)
-                this.setState({
-                    dataSource: this.ds.cloneWithRows(results)
-                });
-            })
-            .catch(error => console.log(`Error fetching photo JSON: ${error}`));
-    }
+}
+getFavedPhotoDataToJson() {
+    getFavedPhotoData(getFavedPhotos())
+        .then(results => {
+            console.log(results)
+            this.setState({
+                dataSource: this.ds.cloneWithRows(results)
+            });
+        })
+        .catch(error => console.log(`Error fetching photo JSON: ${error}`));
+}
 }
 
 export default FavesContainer;
