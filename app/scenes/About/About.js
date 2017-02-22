e537030a236d49bc28bebf530cd60508567a33ac
fergmac <f.macconnell@gmail.com>
2017-02-21 17:20:18 -0800

    style About scene

--- app/scenes/About/About.js
+++ app/scenes/About/About.js
@@ -4,26 +4,34 @@ import {
     Text,
     Image,
 } from 'react-native';
+import { styles } from './styles';
 
 
 const About = () => {
     return (
-      <View>
-        <Image
-          resizeMode={'stretch'}
-          source={require('../../images/red-logo.png')}
-        />
-        <Text>Splashify Mobile App</Text>
-        <Text>Application Version: 1.0</Text>
-        <Text>2016 RED Academy. All rights reserved.</Text>
-        <Text>Credits:</Text>
-        <Text>Splashify was built using the Unsplash API, with help from the following:</Text>
-        <Text>ex-navigation</Text>
-        <Text>react-native</Text>
-        <Text>react-native-timeago</Text>
-        <Text>react-native-vector-icons</Text>
-        <Text>realm-js</Text>
-        <Text>unsplash-js</Text>
+      <View style={styles.container}>
+        <View style={styles.project}>
+          <Image
+            // res
+            style={styles.photo}
+            source={require('../../images/red-logo.png')}
+          />
+          <View style={styles.versionContainer}>
+            <Text style={styles.version}>Splashify Mobile App</Text>
+            <Text style={styles.version}>Application Version: <Text style={styles.strong}>1.0</Text></Text>
+          </View>
+          <Text style={styles.rows}>2016 RED Academy. All rights reserved.</Text>
+        </View>
+        <View style={styles.credit}>
+          <Text style={styles.rows}><Text style={styles.strong}>Credits:</Text></Text>
+          <Text style={styles.rows}>Splashify was built using the Unsplash API, with help from the following:</Text>
+          <Text style={styles.tools}>ex-navigation</Text>
+          <Text style={styles.tools}>react-native</Text>
+          <Text style={styles.tools}>react-native-timeago</Text>
+          <Text style={styles.tools}>react-native-vector-icons</Text>
+          <Text style={styles.tools}>realm-js</Text>
+          <Text style={styles.tools}>unsplash-js</Text>
+        </View>
       </View>
     )
 }
--- app/scenes/About/styles.js
+++ app/scenes/About/styles.js
@@ -3,9 +3,41 @@ import { StyleSheet, Dimensions } from 'react-native';
 export const styles = StyleSheet.create({
     container: {
         flex: 1,
+        margin: 5,
+    },
+    project: {
+        flexDirection: 'column',
+        marginTop: 5,
+        marginBottom: 5,
+    },
+    versionContainer: {
+        marginTop: 10,
+        marginBottom: 10,
+    },
+    version: {
+        fontSize: 18,
+    },
+    rows: {
+        marginTop: 10,
+        marginBottom: 10,
+        fontSize: 18,
+    },
+    tools: {
+        fontWeight: 'bold',
+        color: 'grey',
+        marginTop: 10,
+        marginBottom: 10,
+        fontSize: 14,
     },
     photo: {
         width: Dimensions.get('window').width,
-        height: Dimensions.get('window').width,
-    }
+        marginBottom: 30,
+    },
+    credit: {
+        borderTopWidth: 1,
+        borderTopColor: 'lightgrey',
+    },
+    strong: {
+        fontWeight: 'bold',
+    },
 })
